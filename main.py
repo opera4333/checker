d93c6e94268a5803b354072e01fa519ed963ac98
opera4333 <operam204@gmail.com>
2025-03-31 00:07:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from gatet import Tele  # Import the Tele function from gatet.py
 
 # Bot configuration
 TOKEN = "(7107039898:AAGvH9P6i_4AyZZuUOUVIVHqBR8XbLzBrho)"  # Replace with your bot token
-OWNER_ID = ('1419969308','5792543632')  # Replace with your owner ID
+OWNER_ID = (1419969308)  # Replace with your owner ID
 
 # Initialize the bot
 bot = telebot.TeleBot(TOKEN, parse_mode="HTML")
